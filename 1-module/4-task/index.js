45a16f24b43fca575a64d2d978219ccef5d2c38f
Anton Balashov <balashov.anton.1983@gmail.com>
2021-01-19 22:52:21 +0200

    1-module Add module tasks

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,18 @@
  * @param {string} str base
  * @returns {boolean}
  */
+const SPAM_PATTERN_1 = "1xBet";
+const SPAM_PATTERN_2 = "XXX";
+
 function checkSpam(str) {
-  // ваш код...
+  if (typeof str !== "string") {
+    throw new Error("argument should be a string");
+  }
+
+  const lcStr = str.toLowerCase();
+
+  return (
+    lcStr.includes(SPAM_PATTERN_1.toLowerCase()) ||
+    lcStr.includes(SPAM_PATTERN_2.toLowerCase())
+  );
 }
