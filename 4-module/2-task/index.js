d25ebebfa5b67b72c964ad6c9e095a6d8f51b20a
Anton Balashov <balashov.anton.1983@gmail.com>
2021-02-28 20:51:11 +0200

    4-module Add module tasks

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,5 +2,16 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
+"use strict";
+
 function makeDiagonalRed(table) {
+  if (!table instanceof HTMLTableElement) {
+    throw new Error("argument must be an instance of HTMLTableElement");
+  }
+
+  for (const row of table.rows) {
+    const diagonalCell = row.cells[row.rowIndex];
+
+    diagonalCell.style.backgroundColor = "red";
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,5 +2,38 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
+"use strict";
+
 function highlight(table) {
+  if (!table instanceof HTMLTableElement) {
+    throw new Error("argument must be an instance of HTMLTableElement");
+  }
+
+  for (const row of table.tBodies[0].children) {
+    const available = row.cells[3].dataset.available;
+    const gender = row.cells[2].innerHTML;
+    const age = Number(row.cells[1].innerHTML);
+
+    if (!available) {
+      row.hidden = true;
+    } else {
+      if (available === "true") {
+        row.classList.add("available");
+      }
+
+      if (available === "false") {
+        row.classList.add("unavailable");
+      }
+    }
+
+    if (gender === "m") {
+      row.classList.add("male");
+    } else if (gender === "f") {
+      row.classList.add("female");
+    }
+
+    if (age && age < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
